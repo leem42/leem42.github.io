96912ec9bf196d26a7d35297bb21e7c087950831
Michael Lee <mtlee@michaels-mbp.fhcrc.org>
2017-11-13 09:28:25 -0800

    added test landing page for download of app

---
+++ src/components/TestDeepLink.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app>
+    <div class="row mt-5">
+
+      <div class="col-md-8 mx-auto">
+        <div class="row">
+          <div class="card col-md-12 p-0">
+            <div class="card-body">
+              <h3 class="card-title">
+                <strong>Web View of Test App Download Page</strong>
+              </h3>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Hello, this website serves to showcase projects and some of the work that I\'ve completed'
+    }
+  },
+  methods: {
+    emailRedirect: function () {
+      window.location.href = 'mailto:michael.leem42@gmail.com'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+h1, h2 {
+  font-weight: normal;
+}
+
+.dark-text {
+  color: black;
+}
+
+p.medium {
+  font-size: 1rem * 2.5 !important;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.fillParent {
+  width: inherit;
+  height: inherit;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import TestDeepLink from '@/components/TestDeepLink'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'test ',
+      component: TestDeepLink
     }
   ]
 })
