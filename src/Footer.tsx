3e687148f39f48a1c3b26ffb5485cffd689709b5
Michael Lee <michael.leem42@gmail.com>
2020-06-07 13:41:16 -0700

    update last updated text

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,7 @@ class Footer extends React.Component {
   public render() {
     return (
       <footer style={{position: "fixed", bottom: "0", width: "100%", zIndex: 3, background: "#f5f5f5"}}>
-        <p className="mediumText">Last updated: 11/4/2018 </p>
+        <p className="mediumText">Last updated: 6/7/2020 </p>
       </footer>
     );
   }
